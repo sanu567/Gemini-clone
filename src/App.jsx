4d39182abe7e715f63ad1d74bbf1473266372ad1
unknown <sanusingh5607@gamil.com>
2025-01-11 12:15:56 +0530

    Your commit message

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidebar from './Component/sidecomponent/sidebar'
+import Main from './Component/Maincomponent/Main'
+
+const App = () => {
+  return (
+   <>
+   <Sidebar/>
+   <Main/>
+   </>
+  )
+}
+
+export default App
+
